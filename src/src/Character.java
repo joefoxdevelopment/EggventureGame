6ad1d8139b6f060ec1ae230780268ecfd43e7cad
Andrew Flint <u1460287@hud.ac.uk>
2018-03-28 15:03:46 +0100

    Abstracted the character class, and added attributes.

--- src/src/Character.java
+++ src/src/Character.java
@@ -2,7 +2,31 @@
  * Created by u1354482 on 28/03/2018.
  */
 public abstract class Character {
-    private int health;
-    private int attack;
-    private int defense;
+    protected int health;
+    protected int attack;
+    protected int defense;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
 }
