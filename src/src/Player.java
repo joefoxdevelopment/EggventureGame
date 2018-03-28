f1ebde4626aae22c33336d004859c26099386384
Andrew Flint <u1460287@hud.ac.uk>
2018-03-28 15:06:58 +0100

    Abstracted the character class, and added attributes.

--- src/src/Player.java
+++ src/src/Player.java
@@ -2,4 +2,17 @@
  * Created by u1354482 on 28/03/2018.
  */
 public class Player extends Character {
+
+    private Node location;
+    private int eggCount;
+    //TODO establish how the character's class comes into play, and how this is stored
+
+
+    public Player(){
+        health
+        eggCount = 0;
+    }
+
+    //TODO POSSIBLY SPECIALISATION?
+    //TODO POSSIBLY ABILITIES?
 }
